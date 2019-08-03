bc654989a8bcbd2a39718d36a294c2592deb9c21
ranckprogram <1474574022@qq.com>
2019-08-03 17:07:54 +0800

    init

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
+function Router() {
+    return (
+        <div>
+            router
+        </div>
+    );
+}
+
+export default Router
---
+++ src/views/ChatList/index.js
@@ -0,0 +1,11 @@
+import React, { } from 'react';
+
+function ChatList() {
+    return (
+        <div>
+            ChatList
+        </div>
+    )
+}
+
+export default ChatList;

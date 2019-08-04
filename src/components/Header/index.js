8e9f8cefa0a52de50f86ed7323013b64911a2717
ranckprogram <1474574022@qq.com>
2019-08-04 18:30:21 +0800

    细化Header组件80%

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import styles from './style.module.css'
 function Header (props) {
+    const hasBack = false;
     return (
         <div className={styles.header}>
-            <span>5</span>
-            <h3>微信</h3>
-            <div>
-
+            {hasBack && <i className={`${styles.icon} ${styles.back}`} >&lt;</i>}
+            <h3 className={styles.title}>微信</h3>
+            <input style={{ display: 'none' }} className={styles.input} placeholder="搜索" />
+            <div className={styles.headerRight}>
+                <i className={styles.icon}>s</i>
+                <i className={styles.icon}>+</i>
+                <i className={styles.icon}>...</i>
             </div>
-            <span>
-
-            </span>
         </div>
     );
 }
--- src/views/ChatList/index.js
+++ src/views/ChatList/index.js
@@ -1,148 +1,37 @@
 import React, { } from 'react';
 import styles from './styles.module.css'
 import Avatar from '@/components/Avatar';
+import List from '@/components/List';
 
 function ChatList () {
+    const data = [{
+        id: 1,
+        title: 'ranck',
+        content: '我最近对小念念心动了，怎么办，她都不约我'
+    },
+    {
+        id: 2,
+        title: 'sofy',
+        content: '我最近对小念念心动了，怎么办，她都不约我',
+        time: '9:00',
+    }, {
+        id: 3,
+        title: 'nian',
+    }];
+    
     return (
-        <div className={styles.content}>
-            <div className={styles.list}>
-                <ul>
-                    <li>
-                        <div className={styles.item}>
-                            <div className={styles.avatar}>
-                                <Avatar />
-                            </div>
-                            <div className={styles.itemMiddle}>
-                                <h3>ranck</h3>
-                                <p>content</p>
-                                <time>9:10</time>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={styles.item}>
-                            <div className={styles.avatar}>
-                                <Avatar />
-                            </div>
-                            <div className={styles.itemMiddle}>
-                                <h3>ranck</h3>
-                                <p>content</p>
-                                <time>9:10</time>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={styles.item}>
-                            <div className={styles.avatar}>
-                                <Avatar />
-                            </div>
-                            <div className={styles.itemMiddle}>
-                                <h3>ranck</h3>
-                                <p>content</p>
-                                <time>9:10</time>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={styles.item}>
-                            <div className={styles.avatar}>
-                                <Avatar />
-                            </div>
-                            <div className={styles.itemMiddle}>
-                                <h3>ranck</h3>
-                                <p>content</p>
-                                <time>9:10</time>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={styles.item}>
-                            <div className={styles.avatar}>
-                                <Avatar />
-                            </div>
-                            <div className={styles.itemMiddle}>
-                                <h3>ranck</h3>
-                                <p>content</p>
-                                <time>9:10</time>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={styles.item}>
-                            <div className={styles.avatar}>
-                                <Avatar />
-                            </div>
-                            <div className={styles.itemMiddle}>
-                                <h3>ranck</h3>
-                                <p>content</p>
-                                <time>9:10</time>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={styles.item}>
-                            <div className={styles.avatar}>
-                                <Avatar />
-                            </div>
-                            <div className={styles.itemMiddle}>
-                                <h3>ranck</h3>
-                                <p>content</p>
-                                <time>9:10</time>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={styles.item}>
-                            <div className={styles.avatar}>
-                                <Avatar />
-                            </div>
-                            <div className={styles.itemMiddle}>
-                                <h3>ranck</h3>
-                                <p>content</p>
-                                <time>9:10</time>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={styles.item}>
-                            <div className={styles.avatar}>
-                                <Avatar />
-                            </div>
-                            <div className={styles.itemMiddle}>
-                                <h3>ranck</h3>
-                                <p>content</p>
-                                <time>9:10</time>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={styles.item}>
-                            <div className={styles.avatar}>
-                                <Avatar />
-                            </div>
-                            <div className={styles.itemMiddle}>
-                                <h3>ranck</h3>
-                                <p>content</p>
-                                <time>9:10</time>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className={styles.item}>
-                            <div className={styles.avatar}>
-                                <Avatar />
-                            </div>
-                            <div className={styles.itemMiddle}>
-                                <h3>ranck</h3>
-                                <p>content</p>
-                                <time>9:10</time>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
+        <List
+            dataSource={data}
+            renderItem={item => <List.Item data={1}>
+                <List.Item.Meta
+                    avatar={<Avatar size="middle" />}
+                    title={item.title}
+                    content={item.content}
+                    time={item.time}
+                />
+            </List.Item>}
+        />
+    );
 }
 
 export default ChatList;
